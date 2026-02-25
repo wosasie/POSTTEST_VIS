de83fdc0baaf1012bc4d05c1b4376d3f2fe48a1b
wosasie <i44aul@gmail.com>
2026-02-26 01:10:45 +0800

    testie dua

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,10 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(162, 116)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(162, 241)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(295, 169)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Tambah"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(419, 169)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 3
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(295, 109)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(218, 27)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
 
 End Class
